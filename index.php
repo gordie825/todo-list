bb98913d1c487bdfa4587845f3f5ac895ee80aaa
Gordie825 <Gordie825@gmail.com>
2019-09-01 15:38:02 +0200

    added bootstrap container, row and three columns

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 </head>
 <body>
-    
+<div class="text-center mt-5 container">
+        <div class="row">
+            <div class="col-sm-2"></div>
+                <div class="col-sm-8">
 </body>
 </html>
