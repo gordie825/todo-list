f25887e47aeea2fd8f8acbfd90f8c62b82da8255
Gordie825 <Gordie825@gmail.com>
2019-09-01 15:55:31 +0200

    code for line through and none

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
     var item = 0;
     $("li").each(function(c){
+         // A Call For Any Item That Has Been Clicked In The List
+         $(this).click(function(){
+            checked = c;
+             //check the status of li before setting text-decoration
+             if(item == 0){
+                $(this).css("text-decoration", "line-through");
+                item = 1;
+                sessionStorage.setItem(checked, item);
+     
+               }else{
+                   $(this).css("text-decoration", "none");
+                   item = 0;
+                   sessionStorage.setItem(checked, item);
+                }
+            });
+        });
