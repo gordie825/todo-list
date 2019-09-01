15c36658c6437fa13dc5ad06d7cd3a7ad75245af
Gordie825 <Gordie825@gmail.com>
2019-09-01 15:46:53 +0200

    added comments

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<!-- Session Start -->
+<?php
+    session_start();
+    // Getting Data For Actions In ToDo
+    if(isset($_GET['to'])){
+        $key = $_GET['to'];
+        if($_GET['action'] == 'done'){
+           $_SESSION['todo'][$key]['done']=true; 
+        }else if($_GET['action'] == 'cancel'){
+          $_SESSION['todo'][$key]['done']=false;   
+        }else{
+            unset( $_SESSION['todo'][$key]);
+        }
+    }
+   
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
