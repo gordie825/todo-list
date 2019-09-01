c847f68815225bd92537d986a8213a094d6754a5
Gordie825 <Gordie825@gmail.com>
2019-09-01 15:42:41 +0200

    added scripts and ajax libraries

--- index.php
+++ index.php
@@ -47,5 +47,15 @@
                 <div class="col-sm-2"></div>
         </div>
     </div>
+    <!-- End Bootstrap Columns And Following Naming Convention To Align The Items In The Centre Of The Page -->
+
+    <!-- Start Of Scripts -->
+    <!-- Script Libraries -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script> 
+    <!-- Main JS -->
+    <script src="js/main.js"></script>
+    <!-- End Of Scripts -->
 </body>
 </html>
