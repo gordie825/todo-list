3f284cde97659f29c678c2a5f8661bb112cbdc9e
Gordie825 <Gordie825@gmail.com>
2019-09-03 14:28:27 +0200

    Update todo.php
    Added comments

--- todo.php
+++ todo.php
@@ -1,17 +1,20 @@
 <?php
 session_start();
 // unset($_SESSION['todo']);
+// creating variables to use in index.php
 $todo = $_POST['todo_input'];
 $date = $_POST['todo_date'];
 $submit = $_POST['submit'];
 $delete_todo = $_GET['delete'];
 
+// store all the variables in an array so we can use foreach loop
 $_SESSION['todo'][] = [
     "todo_item" => $todo,
     "todo_dates" => $date,
     "delete" => $delete_todo
 ];
 
+// importing the variables to index.php
 if (isset($todo, $date)) {
     header("location:index.php");
 }
